afac2654df830239bd7a20aafd7af02867e8e8e4
marciely <marcytech@gmail.com>
2022-12-06 20:40:08 -0300

    Criando o schema no projeto

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,10 +5,10 @@ const ProductController = require('../controllers/ProductController')
 
 productRoutes.get('/products/create', ProductController.createProduct)
 productRoutes.post('/products/create', ProductController.createProductPost)
-productRoutes.post('/products/remove/:id', ProductController.removeProduct)
-productRoutes.post('/products/edit', ProductController.editProductPost)
-productRoutes.get('/products/edit/:id', ProductController.editProduct)
-productRoutes.get('/products/:id', ProductController.getProduct)
-productRoutes.get('/products', ProductController.showProducts)
+// productRoutes.post('/products/remove/:id', ProductController.removeProduct)
+// productRoutes.post('/products/edit', ProductController.editProductPost)
+// productRoutes.get('/products/edit/:id', ProductController.editProduct)
+// productRoutes.get('/products/:id', ProductController.getProduct)
+// productRoutes.get('/products', ProductController.showProducts)
 
 module.exports = productRoutes
